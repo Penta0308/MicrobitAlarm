687bbbaa558b25f614a3bc7bb08941f098f2253a
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2019-10-25 01:47:59 +0900

    Update HomeFragment.java

--- app/src/main/java/tk/skmserver/microbitalarm/ui/home/HomeFragment.java
+++ app/src/main/java/tk/skmserver/microbitalarm/ui/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package tk.skmserver.microbitalarm.ui.home;
 
+import android.bluetooth.*;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.BinderThread;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,17 +22,21 @@ import androidx.lifecycle.ViewModelProviders;
 import tk.skmserver.microbitalarm.MainActivity;
 import tk.skmserver.microbitalarm.R;
 
-class colorSwitchListener implements CompoundButton.OnCheckedChangeListener{
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked)
-            textView.setTextColor(Color.RED);
-        else
-            textView.setTextColor(Color.BLACK);
-    }
-}
+import static tk.skmserver.microbitalarm.MainActivity.REQUEST_ENABLE_BT;
 
 public class HomeFragment extends Fragment {
+
+    public TextView BTstate;
+
+    class colorSwitchListener implements CompoundButton.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked)
+                BTstate.setTextColor(Color.RED);
+            else
+                BTstate.setTextColor(Color.BLACK);
+        }
+    }
 
     private HomeViewModel homeViewModel;
 
@@ -45,7 +53,42 @@ public class HomeFragment extends Fragment {
             }
         });
         Switch connect = root.findViewById(R.id.switch_connect);
+        BTstate = root.findViewById(R.id.textView_BTstate);
+
         connect.setOnCheckedChangeListener(new colorSwitchListener());
+// 블루투스 활성화하기
+
+        MainActivity.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 블루투스 어댑터를 디폴트 어댑터로 설정
+
+
+
+        if(MainActivity.bluetoothAdapter == null) { // 디바이스가 블루투스를 지원하지 않을 때
+
+            // 여기에 처리 할 코드를 작성하세요.
+
+        }
+
+        else { // 디바이스가 블루투스를 지원 할 때
+
+            if(MainActivity.bluetoothAdapter.isEnabled()) { // 블루투스가 활성화 상태 (기기에 블루투스가 켜져있음)
+
+                MainActivity.selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+
+            }
+
+            else { // 블루투스가 비 활성화 상태 (기기에 블루투스가 꺼져있음)
+
+                // 블루투스를 활성화 하기 위한 다이얼로그 출력
+
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+                // 선택한 값이 onActivityResult 함수에서 콜백된다.
+
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+
+            }
+
+        }
 
         return root;
     }
