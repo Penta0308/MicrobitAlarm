c04f1ef17e6e400a3cedeca9315bc461e22ab19f
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2019-10-25 22:36:34 +0900

    Update MainActivity.java

--- app/src/main/java/tk/skmserver/microbitalarm/MainActivity.java
+++ app/src/main/java/tk/skmserver/microbitalarm/MainActivity.java
@@ -1,47 +1,56 @@
 package tk.skmserver.microbitalarm;
 
-import android.bluetooth.*;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+
+import com.harrysoft.androidbluetoothserial.BluetoothManager;
 
 public class MainActivity extends AppCompatActivity {
-    public static BluetoothSerial bluetoothSerial;
+
+    public TextView textBTstate;
+    public static BluetoothManager bluetoothManager;
+
+    class colorSwitchListener implements CompoundButton.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked) {
+                textBTstate.setText("Connected");
+            } else {
+                textBTstate.setText("Disconnected");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_music)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-        bluetoothSerial = new BluetoothSerial(this, new BluetoothSerial.MessageHandler() {
-            @Override
-            public int read(int bufferSize, byte[] buffer) {
-                //TODO:read data
-                return bufferSize;
-            }
-        }, getString(R.string.device_prefix));
+
+        bluetoothManager = BluetoothManager.getInstance();
+        if (bluetoothManager == null) {
+            // Bluetooth unavailable on this device :( tell the user
+            Toast.makeText(getApplicationContext(), "Bluetooth not available.", Toast.LENGTH_LONG).show(); // Replace context with your context instance.
+            finish();
+        }
+
+        Switch connect = findViewById(R.id.switch_connect);
+        connect.setOnCheckedChangeListener(new colorSwitchListener());
+
+        textBTstate = findViewById(R.id.textView_btstate);
+
+        List<BluetoothDevice> pairedDevices = MainActivity.bluetoothManager.getPairedDevicesList();
+        for (BluetoothDevice device : pairedDevices) {
+            Log.d("My Bluetooth App", "Device name: " + device.getName());
+            Log.d("My Bluetooth App", "Device MAC Address: " + device.getAddress());
+        }
     }
 }
