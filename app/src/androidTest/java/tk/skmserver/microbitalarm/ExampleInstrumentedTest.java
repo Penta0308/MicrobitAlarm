b6382099b0b59d6bd6b130d75f62d0ef8e31bc3a
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2019-10-25 22:36:20 +0900

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/tk/skmserver/microbitalarm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/tk/skmserver/microbitalarm/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package tk.skmserver.microbitalarm;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
